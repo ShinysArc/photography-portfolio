fce996bc569e0e2af5d1a16a75863059aafb34ed
Stephane Gelibert <stephane.gelibert@epita.fr>
2022-11-27 19:10:58 +0100

    fix: update wbeiste info

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Portfolio - Stéphane Gelibert'
+export const SITE_DESCRIPTION = 'Portfolio pour mes projets en photgraphie.'
+export const HOMEPAGE_URL = 'https://www.stephanegelibert.com/'
