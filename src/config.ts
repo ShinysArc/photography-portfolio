f7966f554f5350e07c00fa20b43f7f4b8ce6710d
Stephane Gelibert <stephane.gelibert@epita.fr>
2023-05-10 02:12:00 +0200

    feat: starting over with a hugo template

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-export const SITE_TITLE = 'Portfolio - Stéphane Gelibert'
-export const SITE_DESCRIPTION = 'Portfolio pour mes projets en photographie.'
-export const HOMEPAGE_URL = 'https://www.stephanegelibert.com/'
